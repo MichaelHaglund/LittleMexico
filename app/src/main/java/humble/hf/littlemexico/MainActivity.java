dc84985e9c04f233ca692aede68741854aef55f1
michael <crash21jr@gmail.com>
2019-06-27 00:23:15 -0700

    adding member vars / sound

--- app/src/main/java/humble/hf/littlemexico/MainActivity.java
+++ app/src/main/java/humble/hf/littlemexico/MainActivity.java
@@ -9,5 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
+
+    public int i;
+
+    // Sound Constants
+    private final int NR_OF_SIMULTANEOUS_SOUNDS = 1;
+    private final float LEFT_VOLUME = 1.0f;
+    private final float RIGHT_VOLUME = 1.0f;
+    private final int NO_LOOP = 0;
+    private final int PRIORITY = 0;
+    private final float NORMAL_PLAY_RATE = 1.0f;
+
+    // Member Variables
+
+
+
+
 }
